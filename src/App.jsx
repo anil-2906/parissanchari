0eb2e9c525ad556c44474b431213753a56d8fe89
anil-2906 <anilbollagoni@gmail.com>
2026-05-08 00:03:36 +0200

    added testimonials section

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,45 @@
 // App.jsx
+
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
 import Packages from "./components/Packages";
 import WhyUS from "./components/WhyUS";
+import Testimonials from "./components/Testimonials"; // ✅ NEW
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import Testimonials from "./components/testimonals";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
-      
-      {/* Prathi component ki oka div wrap chesi ID ivvandi */}
+
+      {/* SERVICES */}
       <div id="services">
         <Services />
       </div>
-      
+
+      {/* PACKAGES */}
       <div id="packages">
         <Packages />
       </div>
-      
+
+      {/* WHY US */}
       <div id="whyus">
         <WhyUS />
       </div>
 
+      {/* TESTIMONIALS / REELS */}
+      <div id="testimonials">
+        <Testimonials />
+      </div>
+
+      {/* CONTACT */}
       <div id="contact">
         <Contact />
       </div>
-      
+
       <Footer />
     </div>
   );
